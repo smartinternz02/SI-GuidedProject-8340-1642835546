57511cfd7f4f735bb9737cc56ee5127cc1a303d9
Sharvesh Pareek <68108106+SharveshPareek@users.noreply.github.com>
2022-01-28 17:07:20 +0530

    Add files via upload

---
+++ Nutro-Assistant-main/src/main/java/com/vigneshwaran/NutritionAssistant/AppConstants.java
@@ -0,0 +1,8 @@
+package com.vigneshwaran.NutritionAssistant;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class AppConstants {
+	
+	public static final String API_KEY="";
+}
---
+++ Nutro-Assistant-main/src/test/java/com/vigneshwaran/NutritionAssistant/NutritionAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigneshwaran.NutritionAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
